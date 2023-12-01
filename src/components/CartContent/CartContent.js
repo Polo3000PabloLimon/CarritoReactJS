ba772839f458e0cf09775c01e7c8940418a222ec
Polo3000PabloLimon <152559615+Polo3000PabloLimon@users.noreply.github.com>
2023-12-01 16:13:32 -0600

    Add files via upload

---
+++ src/components/CartContent/CartContent.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { dataContext } from "../Context/DataContext";
+
+import CartElements from "./CartElements";
+import CartTotal from "./CartTotal";
+
+const CartContent = () => {
+
+  const { cart } = useContext(dataContext);
+
+
+  return cart.length > 0 ?(
+  <>
+    <CartElements />
+    <CartTotal/>
+  </>
+  ): (
+  <h2 className="cart-message-center">Tu carrito esta vacío</h2>
+  )
+};
+
+export default CartContent;
